66a1f34d475d8211d34d1e33e75ae17bc035d471
kunal.patel <kunal.patel@carsales.com.au>
2018-03-21 09:46:02 +1100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/auth.guard';
+
+
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ReadmePageComponent } from './ui/readme-page/readme-page.component';
+import { NotesListComponent } from './notes/notes-list/notes-list.component';
+
+import { UserFormComponent } from './ui/user-form/user-form.component';
+import { UserLoginComponent } from './ui/user-login/user-login.component';
+
+const routes: Routes = [
+  { path: '', component: ReadmePageComponent },
+  { path: 'signup', component: UserFormComponent, },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'items', component: ItemsListComponent, canActivate: [AuthGuard]},
+  { path: 'notes', component: NotesListComponent,  canActivate: [AuthGuard] },
+  // uploads are lazy loaded
+  { path: 'uploads', loadChildren: './uploads/shared/upload.module#UploadModule', canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard]
+})
+export class AppRoutingModule { }
